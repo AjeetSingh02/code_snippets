8bbb1664dd2dcf415dccd1b798a9d25e7877d4eb
Ajeet Singh <ajeet.singh.ec14@gmail.com>
2020-04-10 16:15:24 +0000

    Update write_image_whatsapp.py

--- write_image_whatsapp.py
+++ write_image_whatsapp.py
@@ -1,18 +1,34 @@
 from PIL import Image
 
-im = Image.open("capture.JPG")
-width, height = im.size
+# im = Image.open("capture.JPG")
+# width, height = im.size
 
-workbook = xlsxwriter.Workbook('demo.xlsx')
-worksheet = workbook.add_worksheet()
+def save_images(list_of_images, heights, widths, path_of_excel, column, row):
+    workbook = xlsxwriter.Workbook(path_of_excel)
+    worksheet = workbook.add_worksheet()
+    
+    max_height = max(heights)
+    max_width = max(widths)
+    
+    worksheet.set_column(f"{column}:{column}", max_width/2)
+    worksheet.set_row(row-1, max_height)
+    
+    numberOfImages = len(list_of_images)
+    
+    margin = 30
+    last_width = 0 
+    offset = 0
+    
+    for i in range(numberOfImages):
+        offset = offset + last_width + margin
+        last_width = widths[i]
+        worksheet.insert_image(column + str(row), list_of_images[i], {"x_offset": offset, "y_offset":0})
+    
+    workbook.close()
+    
+    
+images_data = ["capture1.JPG", "capture2.JPG","capture3.JPG", "capture4.JPG"]
+heights = [155, 363, 345, 257]
+widths = [253, 253, 431, 171]
 
-numberOfImages = 4
-worksheet.set_column('B:B', width/2)
-worksheet.set_row(2, height/2)
-
-worksheet.insert_image('B3', 'capture.JPG', {"x_offset": 0, "y_offset":0, "x_scale":0.5, "y_scale":0.5})
-worksheet.insert_image('B3', 'capture.JPG', {"x_offset": width, "y_offset":0, "x_scale":0.5, "y_scale":0.5})
-worksheet.insert_image('B3', 'capture.JPG', {"x_offset": width*2, "y_offset":0, "x_scale":0.5, "y_scale":0.5})
-worksheet.insert_image('B3', 'capture.JPG', {"x_offset": width*3, "y_offset":0, "x_scale":0.5, "y_scale":0.5})
-
-workbook.close()
+save_images(images_data, heights, widths, "demo.xlsx", "B", 3)
